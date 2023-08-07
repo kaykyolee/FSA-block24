3afd8c8862cdc09572fa35d678182077999c9bf3
kaykyolee <kleegoma@gmail.com>
2023-08-07 03:14:48 -0400

    updating select a featured puppy

--- puppy-pals-project/src/App.jsx
+++ puppy-pals-project/src/App.jsx
@@ -2,26 +2,43 @@ import {useState } from 'react'
 import {puppyList} from './data.js'
 import './App.css'
 
-
-
-
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
 
-  console.log("puppyList: ", puppyList);
+
+function handleClick() {
+    // some logic here
+    console.log("puppy id: ", featPupId);
+    }
+
+  // console.log("puppyList: ", puppyList);
+
+const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+console.log(featuredPup)
 
   return (
     <>
     <div className="App">
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>;
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
           })
         }
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}    
+    </div>
 
-      </div>
     </>
   )
+
 }
 
 export default App
